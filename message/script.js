82b7a7c8cf38c478eceef9dcf20001f8458171bf
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2026-03-04 23:26:16 +0500

    20260304_mesage pass project (Completed)

---
+++ message/script.js
@@ -0,0 +1,15 @@
+// 20260204 
+
+// console.log("hi")
+const messageInput = document.getElementById('message-input')
+
+messageInput.addEventListener('keydown', function(event){
+if(event.key == 'Enter'){
+     getMessage()
+}
+})
+
+function getMessage(){
+    document.getElementById('message-output').innerHTML = messageInput.value
+    messageInput.value = ""
+}
