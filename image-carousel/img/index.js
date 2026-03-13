0051f2e8bf6b457ddddb3b1551c676bfbbd0ac10
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2026-03-14 03:16:51 +0500

    20260314_imageCrousel Project(completed)

---
+++ image-carousel/img/index.js
@@ -0,0 +1,14 @@
+// import first from './first.png'
+// import second from './second.png'
+// import third from './third.png'
+// import fourth from './fourth.png'
+
+// export const images = {
+//     first: first,
+//     second: second,
+//     third: third,
+//     newName: fourth
+// }
+
+
+
